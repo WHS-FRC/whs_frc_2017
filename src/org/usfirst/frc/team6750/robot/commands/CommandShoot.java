af0e5c432b72f8a9f6d2eda0fa4b94c708f633a7
phank <phank@10.43.204.135>
2017-01-17 17:06:17 -0500

    Added empty shoot command

---
+++ src/org/usfirst/frc/team6750/robot/commands/CommandShoot.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team6750.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class CommandShoot extends Command {
+	@Override
+	protected void execute() {
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+}
