610a0140441d7a931c12be70fbff6e9d1aa86b16
phank <phank@10.43.204.135>
2017-01-17 17:05:40 -0500

    Added empty Agitator and DriveTrain subsystems

---
+++ src/org/usfirst/frc/team6750/robot/systems/Agitator.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team6750.robot.systems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Agitator extends Subsystem {
+	@Override
+	public void initDefaultCommand() {
+	}
+}
---
+++ src/org/usfirst/frc/team6750/robot/systems/DriveTrain.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team6750.robot.systems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+	@Override
+	protected void initDefaultCommand() {
+	}
+}
