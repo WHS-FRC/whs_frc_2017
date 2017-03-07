c4bb2dcdef6618eac69d161dbfbe6099e3174b71
phank <phank@10.43.204.125>
2017-03-07 14:17:30 -0500

    Added tentative plans for autonomous

--- src/org/usfirst/frc/team6750/robot/commands/AutonomousCommandGroup.java
+++ src/org/usfirst/frc/team6750/robot/commands/AutonomousCommandGroup.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team6750.robot.commands;
 
 import org.usfirst.frc.team6750.robot.Settings;
-import org.usfirst.frc.team6750.robot.Settings.Position;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -14,20 +13,38 @@ public class AutonomousCommandGroup extends CommandGroup {
 	public AutonomousCommandGroup() {
 		super("Autonomous Commands");
 
-		if(Settings.STARTING_POSITION == Position.MIDDLE) {
-			//this.addSequential(new CommandDrive(1.1D, 0D, 0.5D)); //Rotate 90 degrees to the left
-			//this.addSequential(new CommandDrive(2D, 0.6D, 0D)); //Drive for 2 seconds (until in same position if started left)
-			//this.addSequential(new CommandDrive(1D, 0D, -0.523D)); //Rotate 90 degrees to the right
+		switch(Settings.STARTING_POSITION) {
+		case MIDDLE: {
+			/*
+			 * 1) Go forward to middle gear axle
+			 * 2) Go backward to deposit gear
+			 * 3) Turn Left
+			 * 4) Go forward (past the air ship)
+			 * 5) Turn right
+			 * 6) Go forward (past the base line)
+			 */
+			break;
 		}
-
-		this.addSequential(new CommandDrive(2D, 0.5D, 0D)); //Drive across the base line
-
-		//Start to load gears
-
-		if(Settings.STARTING_POSITION == Position.LEFT) {
-			//this.addSequential(new CommandDrive(1D, 0D, -0.523D)); //Rotate 90 degrees to the right
-		} else if(Settings.STARTING_POSITION == Position.RIGHT) {
-			//this.addSequential(new CommandDrive(1D, 0D, -0.523D)); //Rotate 90 degrees to the right
+		case LEFT:
+			/*
+			 * 1) Go forward (align with left gear axle)
+			 * 2) Turn ~45 degrees to the right (face gear axle)
+			 * 3) Go forward to left gear axle
+			 * 4) Go backward to deposit gear
+			 * 5) Turn ~45 degrees to the left (face base line)
+			 * 6) Move forward (past the base line)
+			 */
+			break;
+		case RIGHT:
+			/*
+			 * 1) Go forward (align with right gear axle)
+			 * 2) Turn ~45 degrees to the left (face gear axle)
+			 * 3) Go forward to right gear axle
+			 * 4) Go backward to deposit gear
+			 * 5) Turn ~45 degrees to the right (face base line)
+			 * 6) Move forward (past the base line)
+			 */
+			break;
 		}
 	}
 }
